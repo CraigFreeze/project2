abe73caa73e98110bdcf0bcffd97b03bf81b4588
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2024-10-01 22:34:30 -0700

    isSSN complete?

--- validator.js
+++ validator.js
@@ -1,5 +1,14 @@
 const { body, validationResult } = require('express-validator')
 
+const isSSN = (value) => {
+    // SSN format: 3 digits - 2 digits - 4 digits
+    const ssnRegex = /^\d{3}-\d{2}-\d{4}$/;
+    if (!ssnRegex.test(value)) {
+      throw new Error('Invalid SSN format. Expected format is XXX-XX-XXXX.');
+    }
+    return true;
+  };
+
 const clientValidationRules = () => {
   return [
     body('first_name').trim().isLength({ max: 30 }),
@@ -13,13 +22,7 @@ const clientValidationRules = () => {
     body('zip_code').trim().isPostalCode(),
     body('gender').custom(value => { return value == 'Male' || value == 'Female' }),
     body('marital_status').custom(value => { return value == 'Married' || value == 'Divorced' || value == 'Single' || value == 'Widowed' }),
-    body('ssn').custom(
-        // SSN format: 3 digits - 2 digits - 4 digits
-        ß ssnRegex = /^\d{3}-\d{2}-\d{4}$/;
-        if (!ssnRegex.test(value)) {
-          throw new Error('Invalid SSN format. Expected format is XXX-XX-XXXX.');
-        }
-        return true;),
+    body('ssn').custom(isSSN),
     body('medicare_number').is '1HG4321RT2',
     body('aca_id').is null,
     body('created_at').is '2023-01-15T10:35:20Z',
