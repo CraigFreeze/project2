11348b37fc0c60ea8e78dddffa80d4c1f200cc52
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2024-10-09 08:41:20 -0700

    Add middleware file

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const mongodb = require('./data/database.js');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
+const cors = require('cors');
+
 const port = 3000;
 
 // Middle Ware
 app.use(bodyParser.json())
+//Basic express sesssion({..}) initialization.
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true
+}))
+// init passport to use "express-session"
+app.use(passport.initialize())
+//allow passport to use "express-session"
+app.use(passport.session())
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Orgin', '*');
   res.setHeader(
@@ -15,9 +30,38 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
+app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
+app.use(cors({ origin: '*' }))
+app.use('/', require('./routes/index.js')); //Routes
 
-// Routes
-app.use('/', require('./routes/index.js'));
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+},
+  function (accessToken, refreshToken, profile, done) {
+    return done(null, profile)
+  }
+))
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user)
+})
+
+app.get('/', (req, res) => {
+  res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")
+})
+
+app.get('/github/callback', passport.authenticate('github', {
+  failureRedirect: 'api-docs', session: false
+}), 
+(req, res) => {
+  req.session.user = req.user;
+  res.redirect('/');
+})
 
 // Server Start
 mongodb.initDb((err, mongodb) => {
